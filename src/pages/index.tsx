d4407264c7700ad12b9ae65f59789d23bc491979
Francisco Martin <francisco.martin@ambevtech.com.br>
2021-02-28 15:25:23 -0300

    add challengebox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import styles from "../styles/pages/Home.module.css";
 import CompleteChallenges from "../components/CompleteChallenges";
 import Countdown from "../components/Countdown";
 import Head from "next/head";
+import ChallengeBox from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -18,7 +19,9 @@ export default function Home() {
           <CompleteChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
