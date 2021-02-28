4306921943d73317c6b088dcca904ce0ebdb9a70
Francisco Martin <francisco.martin@ambevtech.com.br>
2021-02-28 00:41:05 -0300

    add countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,14 @@ import Profile from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
 import CompleteChallenges from "../components/CompleteChallenges";
 import Countdown from "../components/Countdown";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
